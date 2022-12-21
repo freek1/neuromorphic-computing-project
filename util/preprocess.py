e37d4cb57c073cc10990aec60bcb92e7cc40ea76
Freek Hens <freek.hens@ru.nl>
2022-12-21 16:49:57 +0100

    Added train and test labels

--- util/preprocess.py
+++ util/preprocess.py
@@ -7,13 +7,7 @@ Then the processed files are saved in data/ with .npy extentions.
 
 digit_converter = TIDIGIT_Converter()
 
-# First test with 5 samples
-results_own = digit_converter.convert_tidigit_own('data/TIDIGIT_train.mat', 'train_samples', 20000, 41, 40)
-results_own = np.array(results_own)
-
-# print(results_own.shape)
-
-# Now, converting the train and test sets:
+# Converting the train and test sets:
 results_lib_train_digit = digit_converter.convert_tidigit_lib('data/TIDIGIT_train.mat', 'train_samples', 20000, 41, 40)
 results_lib_test_digit = digit_converter.convert_tidigit_lib('data/TIDIGIT_test.mat', 'test_samples', 20000, 41, 40)
 
@@ -30,3 +24,14 @@ print('saved train')
 handler.save_file('data/results_lib_test_digit.npy', results_lib_test_digit)
 print('saved test')
 
+# Extracting the train and test labels
+train = scipy.io.loadmat('data/TIDIGIT_train.mat')
+train_labels = np.array(train['train_labels'])
+
+test = scipy.io.loadmat('data/TIDIGIT_test.mat')
+test_labels = np.array(test['test_labels'])
+
+handler.save_file('data/train_labels.npy', train_labels)
+print('saved train labels')
+handler.save_file('data/test_labels.npy', test_labels)
+print('saved test labels')
